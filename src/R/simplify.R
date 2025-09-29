6e01c809fe3979c4bfb399d8d6c117223cdcfa40
kueyar95 <josec.delgado.95@gmail.com>
2025-09-29 20:08:05 -0300

    Proyecto base + descarga hourly_42602_2020.csv (EPA AQS NOx 2020)

--- src/R/simplify.R
+++
@@ -1,8 +0,0 @@
-data <- read.csv("data/hourly_42602_2020.csv", sep=",")
-
-write.table(data[,c("State.Code", "County.Code", "Site.Num",
-"Latitude", "Longitude", "Date.Local", "Time.Local",
-"Sample.Measurement", "Method.Type", "Method.Code",
-"Method.Name", "State.Name", "County.Name")],
-file="data/NOX-2020-ext.csv", sep="\t",
-na="", row.names=FALSE)
